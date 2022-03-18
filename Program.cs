761a5c57531bf9a4907e3fbeffcf4118064b11f9
rodo <rodonatanael02@gmail.com>
2022-03-18 17:23:41 +0700

    merge ke 2

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             HaloGeneric.sapaUser("Rodo Idola Nathanael Marbun");
-
+            DataGeneric<String> c = new DataGeneric<String>("1302204075"); ;
+            c.printData();
         }
     }
 
