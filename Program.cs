bc292f203f80bc1739c907b0e6ed8a317f248be3
thewicked02 <100459594+thewicked02@users.noreply.github.com>
2022-03-18 17:24:59 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
-﻿namespace tpmodul4_1302204075
+namespace tpmodul4_1302204075
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            HaloGeneric.sapaUser("Rodo Idola Nathanael Marbun");
+            
+            DataGeneric<String> c = new DataGeneric<String>("1302204075"); ;
+            c.PrintData();
 
         }
     }
-
-
 
 }
