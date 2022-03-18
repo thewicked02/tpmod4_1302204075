f2fa44754336738c77a20ec66c3b6f79adbdc109
rodo <rodonatanael02@gmail.com>
2022-03-18 17:15:39 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ DataGeneric.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302204075
+{
+        public class DataGeneric<T>
+        {
+            private T data;
+
+            public DataGeneric(T data)
+            {
+                this.data = data;
+            }
+
+            public void printData()
+            {
+                Console.WriteLine("Data yang tersimpan adalah : " + this.data);
+            }
+
+        }
+    }
